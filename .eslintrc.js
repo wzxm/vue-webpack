5449ea5207e525e44cf639bff7b4d979dbf3e7b6
wangzhe@ppmoney.com <wangzhe@ppmoney.com>
2016-10-31 17:06:56 +0800

    edit

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
+    ecmaVersion: 6,
     sourceType: 'module'
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
